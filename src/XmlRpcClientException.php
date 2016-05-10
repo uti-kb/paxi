ca8b99dfcc5c143884f72452cf3eac2692e5bfd4
Hubert Kowalski <h.kowalski@hakger.pl>
2016-05-10 14:12:34 +0200

    Fixed exception inheritance

--- src/XmlRpcClientException.php
+++ src/XmlRpcClientException.php
@@ -3,7 +3,7 @@ namespace Hakger\Paxi;
 
 class XmlRpcClientException extends \Exception
 {
-    public function __construct($message, $code, $previous)
+    public function __construct($message, $code = 0, \Exception $previous = null)
     {
         if (is_array($message)) {
             parent::__construct($message['faultString'], $code, $previous);
